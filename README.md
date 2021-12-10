64eab86c08cb30b5711dab469afa3ded178aceef
bergsom <dannieel_berg@hotmail.com>
2021-12-09 21:00:37 -0300

    ttt
    tttt

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Projeto aap web backend para cadastro de usuários e receitas 
+Instalando dependencias
+#npm i express
+#npm i mongoose
+#npm i cors
+#npm i path
+#npm i cookie-parser
+
+instalando o nodemon
+#npm i -d nodemon
--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const express = require('express');
+const express = require("express");
+const cors = require ('cors');
+const mongoose = require('mongoose');
+const PORT = 5000;
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = 5000
+// console com o banco de dados mongoDB
+mongoose.connect('mongodb://localhost:27017/Cookmaster', {
+    useUnifiedTopology: true,
+}, (err) => {
+    if(err){
+        console.log(err)
+    }else
+    console.log('Conectado ao banco de dados')
+});
 
-app,listen(PORT, () => {
-    console.log('Servidor rodando na porta ${PORT}');
+// rota GET para endereço inicial do projeto
+app.get('/index', (request, response) => {
+    return response.status(200).json({
+mensagem: 'você acessou a rota GET /index',
+    });
+});
+
+app.post('/novoUsuario', (req,res) => {
+    const dados = req.body;
+    console.log(dados);
+})
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}`);
 });
